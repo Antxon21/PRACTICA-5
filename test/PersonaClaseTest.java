a30f5edffc414b8973bcc233d1d4668cc2293fda
Antxon21 <154233377+Antxon21@users.noreply.github.com>
2024-02-19 13:49:24 +0100

    Añadimos carpeta de TEST GETTERS

---
+++ test/PersonaClaseTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaClaseTest {
+
+    private String nombre;
+    private Integer edad;
+    private String dni;
+
+    @Test
+
+
+    public void getDni(){
+        System.out.println(this.dni);
+    }
+}
